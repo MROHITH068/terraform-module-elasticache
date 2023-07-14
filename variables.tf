4efd9127204205b8e02d1be59983da4379697e52
Rohith M <rohith.m068@gmail.com>
2023-07-15 01:10:33 +0530

    Adding docdb and elasticache clusters and instances

--- variables.tf
+++ variables.tf
@@ -4,3 +4,10 @@ variable "tags" {}
 variable "subnet_ids" {}
 variable "kms_key_arn" {}
 variable "sg_subnet_cidr" {}
+variable "node_type" {}
+variable "port" {
+  default = 6379
+}
+variable "replicas_per_node_group" {}
+variable "num_node_groups" {}
+variable "vpc_id" {}
