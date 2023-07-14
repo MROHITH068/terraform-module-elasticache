6c1023fd74c831c17817b65f65dbadb8bb152b06
Rohith M <rohith.m068@gmail.com>
2023-07-14 23:51:41 +0530

    Adding docdb and elasticache subnet groups

--- main.tf
+++ main.tf
@@ -3,8 +3,7 @@ resource "aws_elasticache_subnet_group" "main" {
   name       = "${var.component}-${var.env}"
   subnet_ids = var.subnet_ids
 
-  tags = {
-    Name = merge({ Name = "${var.component}-${var.env}" }, var.tags)
-  }
+  tags = merge({ Name = "${var.component}-${var.env}" }, var.tags)
+
 
 }
